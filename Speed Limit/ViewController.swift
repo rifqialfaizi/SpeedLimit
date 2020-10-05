a1d02fae4e74aaadfcb6b6e05d4c7a4614cca624
rifqialfaizi <rifqialfaizii@gmail.com>
2020-10-05 10:43:27 +0700

    Initial Commit

---
+++ Speed Limit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Speed Limit
+//
+//  Created by Rifqi Alfaizi on 05/10/20.
+//  Copyright © 2020 Rifqi Alfaizi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
