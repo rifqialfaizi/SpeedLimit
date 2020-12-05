a295bc2c7438d4ef37b69572d53ef481701bd0bf
rifqialfaizi <rifqialfaizii@gmail.com>
2020-12-05 22:58:35 +0700

    add data cell

--- Speed Limit/Services/Data Services.swift
+++ Speed Limit/Services/Data Services.swift
@@ -26,7 +26,11 @@ class DataService{
     private let history = [
         History(speed: 20, duration: 20,time: "12.20",date: "Sunday, 2020 October 4th",street: "Jl. Tol Jakarta - Cikampek "),
         History(speed: 30, duration: 30,time: "13.20",date: "Sunday, 2020 October 5th",street: "Jl. Tol Jakarta - Cikampek "),
-        History(speed: 40, duration: 40,time: "14.20",date: "Sunday, 2020 October 6th",street: "Jl. Tol Jakarta - Cikampek ")]
+        History(speed: 40, duration: 50,time: "11.20",date: "Sunday, 2020 October 5th",street: "Jl. Tol Jakarta - Cikampek "),
+        History(speed: 50, duration: 80,time: "10.20",date: "Sunday, 2020 October 5th",street: "Jl. Tol Jakarta - Cikampek "),
+        History(speed: 60, duration: 20,time: "12.20",date: "Sunday, 2020 October 5th",street: "Jl. Tol Jakarta - Cikampek "),
+        History(speed: 70, duration: 10,time: "15.20",date: "Sunday, 2020 October 5th",street: "Jl. Tol Jakarta - Cikampek "),
+        History(speed: 80, duration: 60,time: "16.20",date: "Sunday, 2020 October 6th",street: "Jl. Tol Jakarta - Cikampek ")]
     
     func getHistory() -> [History] {
         return history
